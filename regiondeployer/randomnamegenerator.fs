31e656f59276d2dffe63e15e80065a742466000b
sam <mail@samueljenks.me>
2024-04-30 11:50:23 +1200

    fix: randomnamegenerator

--- regiondeployer/randomnamegenerator.fs
+++ regiondeployer/randomnamegenerator.fs
@@ -1,9 +1,17 @@
 ﻿module regiondeployer.randomnamegenerator
 
-open RandomStringGenerator
+open System
+open System.Text
 
-let public createrandomname(prependage:string, totallength:int) : string =
-    let randomamount = totallength - prependage.Length
-    let generator = new StringGenerator()
-    (prependage + generator.GenerateString(randomamount))
+let public createrandomname(prependage:string, length: int) : string =
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let totallength = (prependage.Length + length)
+    let selected: char array = Array.zeroCreate totallength
+    let random = new Random()
 
+    for index in seq { 0 .. 1 .. length } do
+        let randomNum = random.Next(chars.Length)
+        let nextChar = chars[randomNum]
+        Array.set selected index nextChar
+
+    selected.ToString()
