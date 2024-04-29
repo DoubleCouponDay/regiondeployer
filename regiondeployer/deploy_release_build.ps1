8b6886b96d7440f2a4cc81ba48927c9257db92b8
sam <mail@samueljenks.me>
2024-04-30 10:32:09 +1200

    rm: junk

--- regiondeployer/deploy_release_build.ps1
+++
@@ -1,4 +0,0 @@
-cd "./bin/Release"
-./regiondeployer.exe "C:\\Users\\sjsui\Desktop\\git_workshop\\MoonMachineProprietary\\nodejsarchitecture\\regionalfarmer\\dist\\" "C:\\Users\\sjsui\\Desktop\\git_workshop\\MoonMachineProprietary\\nodejsarchitecture\\exchangeadapter\\built\\apicontracts\\" 25 true
-cd "../../"
-Pause
--- regiondeployer/node.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-..\packages\Ncapsulate.Node.0.10.35.1\nodejs\node %*
-@echo on
