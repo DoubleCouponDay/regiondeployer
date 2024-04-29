d6cbd2d6fb9d356a94e30ba52b3446a4728df3d8
sam <mail@samueljenks.me>
2024-04-30 10:14:15 +1200

    refactor: backup before migration

--- regiondeployer/randomnamegenerator.fs
+++
@@ -1,9 +0,0 @@
-﻿module regiondeployer.randomnamegenerator
-
-open RandomStringGenerator
-
-let public createrandomname(prependage:string, totallength:int) : string =
-    let randomamount = totallength - prependage.Length
-    let generator = new StringGenerator()
-    (prependage + generator.GenerateString(randomamount))
-
