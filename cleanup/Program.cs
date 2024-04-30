6c3b83728e893cef2e36c4e231ce3454d226851f
sam <mail@samueljenks.me>
2024-05-01 10:57:51 +1200

    feat: cleanup rewrite

---
+++ cleanup/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using RegionDeployer;
+using RegionDeployer.AzureAuthenticator;
+using RegionDeployer.CloudActions.ResourceGroup;
+using RegionDeployer.DatabaseSeeder;
+using RegionDeployer.DatabaseCleaner;
+using RegionDeployer.Logger;
+using MoonMachine.Credentials;
+using MoonMachine.Infrastructure.Models;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Initialize();
+        ConfigAdapter.Initialize();
+        var currentConfig = ProjectCredentials.Get.MatchingCurrentEnvironment();
+        var currentEnvironment = Enum.GetName(typeof(CurrentEnvironment), currentConfig.CurrentEnvironment);
+        LogToConsoleAndFile("Cleaning deployment and initial state of environment: " + currentEnvironment);
+        DeleteFarmerGroupsMatchingCurrentEnvironmentMode(LogToConsoleAndFile);
+        DatabaseCleaner.RemoveInitialState(LogToConsoleAndFile);
+    }
+
+    private static void Initialize()
+    {
+        // Initialization logic here
+    }
+
+    private static void LogToConsoleAndFile(string message)
+    {
+        // Logging logic here
+    }
+
+    private static void DeleteFarmerGroupsMatchingCurrentEnvironmentMode(Action<string> logFunction)
+    {
+        // Logic to delete farmer groups
+    }
+}
