889a6cb7a958e5ef09d8b88c125be7f671b1aa72
sam <mail@samueljenks.me>
2024-04-30 20:53:22 +1200

    step: file

---
+++ regiondeployer/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace regiondeployer
+{
+    public class LoggingLock
+    {
+        public static readonly object Lock = new object();
+    }
+
+    public class Logger
+    {
+        private readonly string _logFile;
+
+        public Logger()
+        {
+            _logFile = Directory.GetCurrentDirectory() + "/log.txt";
+            using (var init = File.Create(_logFile)) { }
+        }
+
+        public void LogToConsoleAndFile(string message)
+        {
+            lock (LoggingLock.Lock)
+            {
+                Console.WriteLine(message);
+                using (var logFileStream = File.AppendText(_logFile))
+                {
+                    logFileStream.WriteLine(message);
+                }
+            }
+        }
+    }
+}
