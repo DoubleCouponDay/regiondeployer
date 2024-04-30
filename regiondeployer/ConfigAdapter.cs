f39ee6d4c95b18644067b7dd679ea8626059f69e
sam <mail@samueljenks.me>
2024-05-01 10:16:09 +1200

    step: files

---
+++ regiondeployer/ConfigAdapter.cs
@@ -0,0 +1,17 @@
+﻿using MoonMachine.credentials;
+using Microsoft.FSharp.Linq;
+using System;
+using MoonMachine.Infrastructure.models;
+using MoonMachine.Core.interop.models;
+
+public class ConfigAdapter
+{
+    public void Initialise()
+    {
+        #if DEBUG
+            ProjectCredentials.Get.GlobalOverride(new Nullable<mmenvironment>(mmenvironment.debug));
+        #else
+            ProjectCredentials.Get.GlobalOverride(new Nullable<mmenvironment>(mmenvironment.prod));
+        #endif
+    }
+}
---
+++ regiondeployer/ConnectionStringOverrider.cs
@@ -0,0 +1,17 @@
+﻿using MoonMachine.Infrastructure.Identity;
+using System;
+
+public class DatabaseCleaner
+{
+    public ApplicationDbContext OverrideConnectionString(string connectionString = null)
+    {
+        if (!string.IsNullOrEmpty(connectionString))
+        {
+            return new ApplicationDbContext(connectionString);
+        }
+        else
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
