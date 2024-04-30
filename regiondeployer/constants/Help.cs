61cdd758e286c7d46c1080fd7ecc2e358609f8da
sam <mail@samueljenks.me>
2024-05-01 10:20:29 +1200

    step: file

---
+++ regiondeployer/constants/Help.cs
@@ -0,0 +1,13 @@
+﻿namespace RegionDeployer
+{
+    public class Helper
+    {
+        public const string HelpString = 
+            "regional deployer requires 4 arguments:\n" +
+            "    -regionalfarmersrootpath:   Absolute path to the regionalfarmer project's release directory\n" +
+            "    -ContractsFolderPath:       Absolute path to the exchangeadapter project's apicontracts\n" +
+            "    -NumberOfFarmers:           Number of farmers to deploy\n";
+
+        public const string NoArgumentsString = "4 arguments expected, use -h for help";
+    }
+}
