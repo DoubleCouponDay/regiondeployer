1b5d4e680dce632be1a7a630d5fef3649e7d482e
sam <mail@samueljenks.me>
2024-05-01 10:18:46 +1200

    step: file

---
+++ regiondeployer/AzureAuthenticator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using MoonMachine.Infrastructure.Helpers;
+using MoonMachine.credentials;
+
+public class AzureAuthenticator
+{
+    public IAzure CreateAuthenticatedGateway()
+    {
+        var globalConfig = ProjectCredentials.Get.MatchingCurrentEnvironment();
+
+        var credentials = SdkContext.AzureCredentialsFactory
+            .FromServicePrincipal(
+                globalConfig.AzureClientId,
+                globalConfig.AzureClientSecret,
+                globalConfig.AzureTenantId,
+                AzureEnvironment.AzureGlobalCloud);
+
+        var authenticatedAzure = Azure.Authenticate(credentials);
+        return authenticatedAzure.WithDefaultSubscription();
+    }
+}
