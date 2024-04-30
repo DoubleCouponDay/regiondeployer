84b327c4eea7f13afaae03d145bba16bb169151f
sam <mail@samueljenks.me>
2024-05-01 10:24:33 +1200

    step: file

---
+++ regiondeployer/constants/PersonalPaths.cs
@@ -0,0 +1,9 @@
+﻿namespace RegionDeployer
+{
+    public class PersonalPaths
+    {
+        public const string RegionDeployersRoot = @"C:\\Users\\sjsui\\Desktop\\git_workshop\\MoonMachineProprietary\\regiondeployer\\regiondeployer\\";
+        public const string ExchangeAdaptersRoot = @"C:\\Users\\sjsui\\Desktop\\git_workshop\\MoonMachineProprietary\\nodejsarchitecture\\exchangeadapter\\";
+        public const string RegionalFarmersRoot = @"C:\\Users\\sjsui\\Desktop\\git_workshop\\MoonMachineProprietary\\nodejsarchitecture\\regionalfarmer\\dist\\";
+    }
+}
