640df7a8a3298ec92f676df17093ca59dbadd97e
sam <mail@samueljenks.me>
2024-05-01 10:21:40 +1200

    step: file

---
+++ regiondeployer/constants/Independent.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace RegionDeployer
+{
+    public class Independent
+    {
+        public const string ValueSeparator = ": ";
+        public const string IpAccessKeyName = "ipstackkey";
+        public const string AccessKeyParamKey = "?access_key=";
+        public static readonly string PlaceholderIpStackAddress = 
+            @"http://api.ipstack.com/google.com" + AccessKeyParamKey + "26928c3a15335fb85a8418e7dff2c3bf";
+        public const string JsonMimeType = "application/json";
+    }
+}
