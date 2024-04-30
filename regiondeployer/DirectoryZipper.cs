dfc15b7c02a043f42baee19b537714b078015dfd
sam <mail@samueljenks.me>
2024-04-30 20:56:17 +1200

    step: file

---
+++ regiondeployer/DirectoryZipper.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace regiondeployer
+{
+    public class ZipManager
+    {
+        public const string DefaultZipName = "regionfarmer";
+
+        public string ZipUpProject(string projectPath, string outputFileName, Action<string> logMessage)
+        {
+            logMessage($"Zipping project directory... {projectPath}");
+            string output = Path.Combine(Directory.GetCurrentDirectory(), $"{outputFileName}.zip");
+
+            if (File.Exists(output))
+            {
+                File.Delete(output);
+            }
+
+            ZipFile.CreateFromDirectory(projectPath, output);
+            logMessage("directory zipped.");
+            return output;
+        }
+    }
+}
