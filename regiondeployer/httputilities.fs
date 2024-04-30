867def4e7f82a7aeed92a8488bdc07ac020b255d
sam <mail@samueljenks.me>
2024-04-30 17:12:44 +1200

    refactor: class

--- regiondeployer/httputilities.fs
+++ regiondeployer/httputilities.fs
@@ -1,4 +1,4 @@
-module httputilities
+namespace regiondeployer 
 
 open System
 open System.Collections.Generic
@@ -6,38 +6,39 @@ open System.Linq
 open System.Net.Http
 open System.Text
 
-let basicauthseparator = ":"
-let basicauthkey = "Authorization"
-let dummyurl = Uri("http://localhost")
+type httputilities() =
+    let basicauthseparator = ":"
+    let basicauthkey = "Authorization"
+    let dummyurl = Uri("http://localhost")
 
-let getjsoncontent (json: string) =
-    new StringContent(json, Encoding.UTF8, "application/json") // Assuming coreconstants.jsonmimetype is "application/json"
+    member this.getjsoncontent (json: string) =
+        new StringContent(json, Encoding.UTF8, "application/json") // Assuming coreconstants.jsonmimetype is "application/json"
 
-let createrequestmessage (url: string, method: HttpMethod, content: HttpContent, headers: KeyValuePair<string, string>[]) =
-    let request = new HttpRequestMessage(method, Uri(url))
-    request.Content <- content
-    headers |> Array.iter (fun kvp -> request.Headers.Add(kvp.Key, kvp.Value))
-    request
+    member this.createrequestmessage (url: string, method: HttpMethod, content: HttpContent, headers: KeyValuePair<string, string>[]) =
+        let request = new HttpRequestMessage(method, Uri(url))
+        request.Content <- content
+        headers |> Array.iter (fun kvp -> request.Headers.Add(kvp.Key, kvp.Value))
+        request
 
-let Clone (req: HttpRequestMessage) =
-    let clone = new HttpRequestMessage(req.Method, req.RequestUri)
-    clone.Content <- req.Content
-    clone.Version <- req.Version
-    let sequence = req.Options.AsEnumerable() 
-    
-    Seq.iter(fun (current : KeyValuePair<string, obj>) -> 
-        clone.Options.Append(new KeyValuePair<string, obj>(current.Key, current.Value)) |> ignore)
-        sequence
-    
-    let oldheaders: List<KeyValuePair<string, IEnumerable<string>>> = req.Headers.ToList()
-    
-    for current in oldheaders do
-        clone.Headers.TryAddWithoutValidation(current.Key, current.Value) |> ignore
+    member this.Clone (req: HttpRequestMessage) =
+        let clone = new HttpRequestMessage(req.Method, req.RequestUri)
+        clone.Content <- req.Content
+        clone.Version <- req.Version
+        let sequence = req.Options.AsEnumerable() 
+        
+        Seq.iter(fun (current : KeyValuePair<string, obj>) -> 
+            clone.Options.Append(new KeyValuePair<string, obj>(current.Key, current.Value)) |> ignore)
+            sequence
+        
+        let oldheaders: List<KeyValuePair<string, IEnumerable<string>>> = req.Headers.ToList()
+        
+        for current in oldheaders do
+            clone.Headers.TryAddWithoutValidation(current.Key, current.Value) |> ignore
 
-    clone
+        clone
 
-let createbasicauthheader (username: string, password: string) =
-    let input = username + basicauthseparator + password
-    let bytes = Encoding.UTF8.GetBytes(input)
-    let output = Convert.ToBase64String(bytes)
-    KeyValuePair<string, string>(basicauthkey, "Basic " + output)
+    member this.createbasicauthheader (username: string, password: string) =
+        let input = username + basicauthseparator + password
+        let bytes = Encoding.UTF8.GetBytes(input)
+        let output = Convert.ToBase64String(bytes)
+        KeyValuePair<string, string>(basicauthkey, "Basic " + output)
