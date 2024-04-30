a2a181226bdbcda58c49643bc2d288f065e8bf9b
sam <mail@samueljenks.me>
2024-05-01 09:39:07 +1200

    step: models

---
+++ regiondeployer/models/mmenvironment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoonMachine.Core.interop.models
+{
+    public enum mmenvironment
+    {
+        debug,
+        prod
+    }
+}
