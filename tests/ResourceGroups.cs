c2b46ee0de000ccfca4fcd3cceff522f1b043d3d
sam <mail@samueljenks.me>
2024-05-01 13:02:54 +1200

    step: resource group tests

---
+++ tests/ResourceGroups.cs
@@ -0,0 +1,47 @@
+﻿namespace tests;
+
+using Xunit;
+using regiondeployer.cloudactions.resourcegroup;
+using Microsoft.Azure.Management.Fluent;
+using System;
+using regiondeployer.clouderror;
+using Xunit.Abstractions;
+using regiondeployer;
+
+public class WhenItCreatesAResourceGroup : IDisposable
+{
+    private string groupname = null;
+    private readonly ITestOutputHelper logger;
+
+    public WhenItCreatesAResourceGroup(ITestOutputHelper inputLogger)
+    {
+        configadapter.initialise();
+        this.logger = inputLogger;
+    }
+
+    public void Dispose()
+    {
+        deleteresourcegroup(this.groupname, logger.WriteLine);
+    }
+
+    [Fact]
+    public string ItSucceeds()
+    {
+        this.groupname = createrandomresourcegroup(defaultregion, logger.WriteLine);
+        return this.groupname;
+    }
+
+    [Fact]
+    public void ItsNameIsSuchALength()
+    {
+        var newregionname = this.ItSucceeds();
+        Assert.True(newregionname.Length == maxgroupnamelength);
+    }
+
+    [Fact]
+    public void ItsNameContainsDefaultGroupName()
+    {
+        var newregionname = this.ItSucceeds();
+        Assert.True(newregionname.Contains(basegroupname));
+    }
+}
