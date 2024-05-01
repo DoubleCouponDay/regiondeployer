58eae20ed7ef9489df3a247e8d0e2517b70e4b47
sam <mail@samueljenks.me>
2024-05-01 13:11:23 +1200

    step: nodeadapting tests

---
+++ tests/NodeAdapting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using MoonMachine.Core;
+using MoonMachine.Infrastructure.Entities;
+using regiondeployer.nodeadapter;
+
+namespace Tests
+{
+    public class WhenTheNodeAdapterGetsMarkets
+    {
+        private string mmexchangespathcontext = regiondeployer.constants.personalpaths.exchangeadaptersroot;
+
+        [Theory]
+        [InlineData(coreconstants.defaultexchangename)]
+        public IEnumerable<AvailableMarket> ItSucceeds(string exchangename)
+        {
+            var result = adapter.getmarkets(exchangename, mmexchangespathcontext);
+            Assert.True(result != null, "a result was given");
+            Assert.True(result.Count() != 0, "some markets were returned");
+            return result;
+        }
+
+        [Fact]
+        public void TheCurrenciesAreNotEmpty()
+        {
+            var exchangenamecontext = coreconstants.defaultexchangename;
+            var adaptercontext = ItSucceeds(exchangenamecontext);
+
+            foreach (var market in adaptercontext)
+            {
+                Assert.False(String.IsNullOrEmpty(market.HoardedCurrency), "hoarded is not empty");
+                Assert.False(String.IsNullOrEmpty(market.PriceCurrency), "price is not empty");
+            }
+        }
+    }
+}
